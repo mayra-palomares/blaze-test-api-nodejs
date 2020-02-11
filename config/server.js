a3d70f9344626842bd3e5348258aacbb3e266e25
Mayra Palomares <mayra.palomares.delacruz@gmail.com>
2020-02-11 13:29:26 -0500

    Change port. Allow cross domain.

--- config/server.js
+++ config/server.js
@@ -8,6 +8,15 @@ require('dotenv').config()
 const app = express()
 app.use(express.json())
 
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+
+    next();
+}
+app.use(allowCrossDomain)
+
 //Dependency Injection config
 const srcDir = path.join(__dirname);
 const options = {serviceFilePath: './config/dependency-injection-config.yml', defaultDir: srcDir + '/../'}
